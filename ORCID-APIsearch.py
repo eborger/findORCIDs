faa296bbed81a1f4acc348b036e62122099caf78
eborger <eb427@st-andrews.ac.uk>
2018-08-02 02:21:30 +0100

    cleaning up

--- ORCID-APIsearch.py
+++ ORCID-APIsearch.py
@@ -10,40 +10,56 @@ from xml.etree import ElementTree
 import os, sys
 import csv, pickle
 
-##Search by "University of St Andrews"
-url = 'https://pub.orcid.org/v2.1/search/?q=affiliation-org-name:"University+of+St+Andrews"&start=401&rows=200'
-response = requests.get(url)
-results = ElementTree.fromstring(response.text)
-
-## Search by "University of Saint Andrews"
-url = 'https://pub.orcid.org/v2.1/search/?q=affiliation-org-name:"University+of+Saint+Andrews"&start=1&rows=200'
-response = requests.get(url)
-results = ElementTree.fromstring(response.text)
-
-with open('ORCIDsearch-SAINT.txt', 'w', encoding='utf-8') as f:
-    f.write(ElementTree.tostring(results).decode("utf-8"))
+def searchORCID(searchterm, ringgold = False, email = False):
+    '''
+    Searches the ORCID registry for ORCID iDs based on the search term entered as string with quoation marks. 
+    Maximum response lengths is 200, so search must be done in stages for retrieving more than 200 IDs.
     
-## Sarch by Ringgold ID 7486
-url = 'https://pub.orcid.org/v2.1/search/?q=ringgold-org-id:7486&start=1&rows=200'
-response = requests.get(url)
-results = ElementTree.fromstring(response.text)
-
-with open('ORCIDsearch-RG.txt', 'w', encoding='utf-8') as f:
-    f.write(ElementTree.tostring(results).decode("utf-8"))
+    Use a search string such as "University of xxx". 
+    If search is done usig ringgold ID, set optional second argument ringold = True. Default is False.
+    If search is done by email domain, set optional third argument email = True. Defaul is False.
     
-
-##Search by email (typically not public), uncomment if needed    
-#url = 'https://pub.orcid.org/v2.1/search/?q=email:*@st-andrews.ac.uk&start=1&rows=200'
-#response = requests.get(url)
-#results = ElementTree.fromstring(response.text)
-#
-#with open('ORCIDsearch-email.txt', 'w', encoding='utf-8') as f:
-#    f.write(ElementTree.tostring(results).decode("utf-8"))
+    Results is an xml tree saved to a text will in the working directory.
+    '''
+    start = 1
+    q = (searchterm, start)
+    IDs = ElementTree.Element("results")
+    
+    if ringgold == True:
+        url = 'https://pub.orcid.org/v2.1/search/?q=ringgold-org-id:%s&start=%s&rows=200' % q
+    elif email == True:
+        url = 'https://pub.orcid.org/v2.1/search/?q=email:%s&start=%s&rows=200' % q
+    else: 
+        url = "https://pub.orcid.org/v2.1/search/?q=affiliation-org-name:%s&start=%s&rows=200" % q
+        
+    response = requests.get(url)
+    results = ElementTree.fromstring(response.text)
+    IDs.append(results)
+    
+    while (len(results) == 200):
+        start += 200
+        q = (searchterm, start)
+        url = "https://pub.orcid.org/v2.1/search/?q=affiliation-org-name:%s&start=%s&rows=200" % q
+        response = requests.get(url)
+        results = ElementTree.fromstring(response.text)
+        IDs.append(results)
+    
+    # exract ORCIDiDs
+    orcids = []
+    for result in output.findall('{http://www.orcid.org/ns/search}result'):
+        for identifier in result.findall('{http://www.orcid.org/ns/common}orcid-identifier'):
+            for orcid in identifier.findall('{http://www.orcid.org/ns/common}path'):
+                orcids.append(orcid.text)
+            
+    with open('ORCID-IDs.txt', 'w', encoding='utf-8') as f:
+        f.write(ElementTree.tostring(IDs).decode("utf-8"))
+    
+    print("Done.")
 
 
 ##Functions to combine individual files downloaded after keyword search:
 def getfiles(path):
-    '''generates a list of full paths for all txt files.'''
+    '''generates a list of full paths for all txt files in the path.'''
     files = []
     for filename in os.listdir(path):
         if filename.endswith('.txt'):
@@ -63,12 +79,22 @@ def combine_xml_files(files):
     if first is not None:
         return first #ElementTree.tostring(first).decode("utf-8") for printing to console
 
+def export_csv(my_dict):
+    with open('ORCIDs_names.csv', 'w', newline='', encoding='utf-8') as f:
+        fieldnames = ['ORCID_iD', 'name', 'lastname']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        data = [dict(zip(fieldnames, [k, v])) for k, v in oricds_names.items()]
+        writer.writerows(data)
+
+
 ##load the xml data into one element
 path = r'C:\Users\eb427\Documents\ORCID\ORCID-APIsearch\20180711'
 files = getfiles(path)
-output = combine_xml_files(files) # yields 1877 ORCID iDs
+output = combine_xml_files(files)
 
 #extract ORCID iDs from the output
+#root.findall('{*/{http://www.orcid.org/ns/search}result') to extract from nested result
 orcids = []
 for result in output.findall('{http://www.orcid.org/ns/search}result'):
     for identifier in result.findall('{http://www.orcid.org/ns/common}orcid-identifier'):
@@ -89,7 +115,7 @@ def match_names(orcid_ids):
     '''
     orcids_names = {}      
     for i in orcids:
-        url = 'https://pub.orcid.org/v2.1/%s/person' %i
+        url = 'https://pub.orcid.org/v2.1/%s/person' % i
         response = requests.get(url)
         result = ElementTree.fromstring(response.text)
         try:
@@ -117,13 +143,6 @@ def match_names(orcid_ids):
 #In [139]: orcids_names
 #Out[139]: {'0000-0003-4965-2969': 'Eva Borger'}
 
-def export_csv(my_dict):
-    with open('ORCIDs_names.csv', 'w', newline='', encoding='utf-8') as f:
-        fieldnames = ['ORCID_iD', 'name', 'lastname']
-        writer = csv.DictWriter(f, fieldnames=fieldnames)
-        writer.writeheader()
-        data = [dict(zip(fieldnames, [k, v])) for k, v in oricds_names.items()]
-        writer.writerows(data)
 
 #Backup: export the dictionary to a pickle file which can be loaded again:
 with open('ORCIDs_names.pkl', 'wb') as f:
